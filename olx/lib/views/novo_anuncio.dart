5a11f2c4f3b7ead5ee6148f6599adc1f7651ace0
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2022-07-25 20:48:26 -0300

    -Selecionando e removendo imagens do anúncio.

--- olx/lib/views/novo_anuncio.dart
+++ olx/lib/views/novo_anuncio.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:olx/views/widgets/botao_customizado.dart';
 import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:olx/views/widgets/botao_customizado.dart';
 
 class NovoAnuncio extends StatefulWidget {
   const NovoAnuncio({Key? key}) : super(key: key);
@@ -11,7 +13,7 @@ class NovoAnuncio extends StatefulWidget {
 
 class _NovoAnuncioState extends State<NovoAnuncio> {
   final _formKey = GlobalKey<FormState>();
-  List<File> _listaImagens = [];
+  List<XFile> _listaImagens = [];
 
   @override
   Widget build(BuildContext context) {
@@ -27,76 +29,113 @@ class _NovoAnuncioState extends State<NovoAnuncio> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                  FormField<List>(
-                    initialValue: _listaImagens,
-                      builder: (state){
-                        return Column(
-                          children: [
-                            Container(
-                              height: 100,
-                              child: ListView.builder(
-                                  itemCount: _listaImagens.length + 1,
-                                  scrollDirection: Axis.horizontal,
-                                  itemBuilder: (context, index){
-                                  if(index == _listaImagens.length){
-                                    return Padding(padding: EdgeInsets.symmetric(horizontal: 8),
-                                      child: GestureDetector(
-                                        onTap: (){
-                                          _selecionarImagemGaleria();
-                                        },
-                                        child: CircleAvatar(
-                                          backgroundColor: Colors.grey[400],
-                                          radius: 50,
-                                          child: Column(
-                                            mainAxisAlignment: MainAxisAlignment.center,
-                                            children: [
-                                              Icon(
-                                                Icons.add_a_photo,
-                                                size: 40,
-                                                color: Colors.grey[100],
-                                              ),
-                                              Text("Adicionar",
+                FormField<List>(
+                  initialValue: _listaImagens,
+                  builder: (state) {
+                    return Column(
+                      children: [
+                        Container(
+                          height: 100,
+                          child: ListView.builder(
+                              itemCount: _listaImagens.length + 1,
+                              scrollDirection: Axis.horizontal,
+                              itemBuilder: (context, index) {
+                                if (index == _listaImagens.length) {
+                                  return Padding(
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 8),
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        _selecionarImagemGaleria();
+                                      },
+                                      child: CircleAvatar(
+                                        backgroundColor: Colors.grey[400],
+                                        radius: 50,
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Icon(
+                                              Icons.add_a_photo,
+                                              size: 40,
+                                              color: Colors.grey[100],
+                                            ),
+                                            Text(
+                                              "Adicionar",
                                               style: TextStyle(
-                                                color: Colors.grey[100]
-                                              ),)
-                                            ],
-                                          ),
+                                                  color: Colors.grey[100]),
+                                            )
+                                          ],
                                         ),
                                       ),
-                                    );
-                                  }
-                                  if(_listaImagens.length > 0){
-
-                                  }
-                                  return Container();
+                                    ),
+                                  );
+                                }
+                                if (_listaImagens.length > 0) {
+                                  return Padding(
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 8),
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        showDialog(context: context, 
+                                            builder: (context)=> Dialog(
+                                              child: Column(
+                                                mainAxisSize: MainAxisSize.min,
+                                                children: [
+                                                  Image.file(File(_listaImagens[index].path)),
+                                                  TextButton(
+                                                      onPressed: (){
+                                                        setState((){
+                                                          _listaImagens.removeAt(index);
+                                                          Navigator.of(context).pop();
+                                                        });
+                                                      },
+                                                      child: Text("Excluir", style: TextStyle(color: Colors.red),))
+                                                ],
+                                              ),
+                                            ));
+                                      },
+                                      child: CircleAvatar(
+                                        radius: 50,
+                                        backgroundImage: FileImage(File(_listaImagens[index].path)),
+                                        child: Container(
+                                          color: Color.fromRGBO(255,255,255,0.4),
+                                          alignment: Alignment.center,
+                                          child: Icon(Icons.delete, color:Colors.red),
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                }
+                                return Container();
                               }),
+                        ),
+                        if (state.hasError)
+                          Container(
+                            child: Text(
+                              "[${state.errorText}]",
+                              style: TextStyle(fontSize: 14, color: Colors.red),
                             ),
-                            if(state.hasError)
-                              Container(
-                                child: Text("[${state.errorText}]", style: TextStyle(fontSize: 14, color: Colors.red),),
-                              ),
-                          ],
-                        );
-                      },
-                    validator: (imagens){
-                      if(imagens!.isEmpty){
-                        return "Necessário selecionar uma imagem";
-                      }
-                      return null;
-                    },
-                  ),
-                  Row(
-                    children: [
-                      Text("Estado"),
-                      Text("Categoria")
-                    ],
-                  ),
+                          ),
+                      ],
+                    );
+                  },
+                  validator: (imagens) {
+                    if (imagens!.isEmpty) {
+                      return "Necessário selecionar uma imagem";
+                    }
+                    return null;
+                  },
+                ),
+                Row(
+                  children: [Text("Estado"), Text("Categoria")],
+                ),
                 Text("Caixas de Texto"),
-                BotaoCustomizado(texto: "Cadastrar anúncio", onPressed: (){
-                  if(_formKey.currentState!.validate()){
-
-                  }
-                }),
+                BotaoCustomizado(
+                    texto: "Cadastrar anúncio",
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {}
+                    }),
               ],
             ),
           ),
@@ -105,7 +144,14 @@ class _NovoAnuncioState extends State<NovoAnuncio> {
     );
   }
 
-  _selecionarImagemGaleria(){
-
+  _selecionarImagemGaleria() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? imagemSelecionada =
+        await _picker.pickImage(source: ImageSource.gallery);
+    if (imagemSelecionada != null) {
+      setState((){
+        _listaImagens.add(imagemSelecionada);
+      });
+    }
   }
 }
